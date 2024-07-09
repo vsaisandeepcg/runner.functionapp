2e7101c3f915eda40c25510c8909e8e8502508ca
vsaisandeepcg <v-sai.sandeep@capgemini.com>
2024-07-09 15:01:56 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
-class Program
+public static class HelloWorldFunction
 {
-    static void Main()
+    [FunctionName("HelloWorldFunction")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        Console.WriteLine("Hello, World!");
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        return new OkObjectResult("Hello, World!");
     }
 }
