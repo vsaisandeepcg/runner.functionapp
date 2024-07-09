764eaf022693f3d2fe41ba8d6a8c5e57de1367f6
anusha414 <53256022+anusha414@users.noreply.github.com>
2024-07-09 21:29:43 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
-class Program
+public static class HelloWorld
 {
-    static void Main()
+    [FunctionName("HelloWorld")]
+    public static IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        Console.WriteLine("Hello, World!");
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string responseMessage = "Hello, world!";
+
+        return new OkObjectResult(responseMessage);
     }
 }
