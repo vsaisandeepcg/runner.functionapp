ffddb60d4a2831cc5cd17c92388fb96a576fdfd9
anusha414 <53256022+anusha414@users.noreply.github.com>
2024-07-09 21:43:36 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,9 @@
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
 
-public static class HelloWorld
+class Program
 {
-    [FunctionName("HelloWorld")]
-    public static IActionResult Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
+    static void Main()
     {
-        log.LogInformation("C# HTTP trigger function processed a request.");
-
-        string name = req.Query["name"];
-
-        string requestBody = new StreamReader(req.Body).ReadToEnd();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-        name = name ?? data?.name;
-
-        return name != null
-            ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        Console.WriteLine("Hello, World!");
     }
 }
